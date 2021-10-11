44bdb7689a0aa7106152a6a8e8d06ee67b71fade
wangrui <wr083025>
2021-10-11 14:05:31 +0800

    首页UI初步； 增加相关依赖；

--- app/src/main/java/com/qt/bluetooth/HomeActivity.java
+++ app/src/main/java/com/qt/bluetooth/HomeActivity.java
@@ -1,13 +1,23 @@
 package com.qt.bluetooth;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gyf.immersionbar.ImmersionBar;
+
 
 public class HomeActivity extends AppCompatActivity {
+    ImmersionBar immersionBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        immersionBar= ImmersionBar.with(this);
+//        immersionBar.init();
     }
+
 }
